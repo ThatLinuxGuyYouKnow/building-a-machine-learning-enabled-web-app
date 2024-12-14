89e3543756b23e52cd0951e104d4470b921ab6ad
Emmanuels-code <arocket04@gmail.com>
2024-12-14 12:54:13 +0100

    chore: removed redundant export

--- src/AmazonML.js
+++ src/AmazonML.js
@@ -10,13 +10,31 @@ const creds = {
   },
 };
 
+
+let rekognitionClient = null;
 export async function analyzeImageML(type, imageData) {
-  const returnData = {
-    type: "warning",
-    text: "This feature has not been implemented yet!",
-  };
+  let returnData = null;
+  try {
+    if (type == "labels") {
+      // If the client has not been initialized yet, create it
+      if (!rekognitionClient)  
+        rekognitionClient = new RekognitionClient(creds); // pass in the creds as parameter
+      returnData = {
+        // This is for test purposes, it will show a message that the client was created
+        type: "info",
+        text: "Rekognition Client Created",
+      };
+    }
+  } catch (error) {
+    returnData = {
+      type: "error" /* success info warning error */,
+      text: error.message,
+    };
+  }
   return JSON.stringify(returnData);
 }
+
+// imageData is string with data:application/octet-stream;base64,...
 
 // imageData is string with data:application/octet-stream;base64,...
 function base64ToUint8Array(base64Data) {
